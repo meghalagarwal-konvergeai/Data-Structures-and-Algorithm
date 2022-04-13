fc75af669afb9a7d57f63789ee316772da6f7a54
Meghal Agarwal <meghal.agarwal@konverge.ai>
2022-04-13 15:07:02 +0530

    Binary Search Training and Practice

--- Binary Search/Assinment 1 Rotational Count Using Binary Serach.py
+++ Binary Search/Assinment 1 Rotational Count Using Binary Serach.py
@@ -3,22 +3,27 @@ Rotational Count Sorting through Binary Search.
 -> First Check the mid value is smaller than predecessor, if yes, then return the count
 -> Second Check the mid value is smaller than the last value of the list then change High = mid-1 (result lies in LeftHand Side of the list) else Low = mid+1 (result lies in RightHand Side of the list)
 '''
-
+#This function first go to the First Position function to find te 1st position and then it will move to Last position
 def number_of_rotations(nums):
     low, high = 0, len(nums)-1
     
     while low <= high:
+        # The middle position is the answer
         mid = (low+high)//2
         mid_number = nums[mid]
-        print({"low":low, "High":high, "Mid":mid, "Number":nums[mid]})
+        
         if(mid > 0 and mid_number <= nums[mid-1]):
             return mid
         elif(mid_number>nums[-1]):
+            # Answer lies in the right half
             low = mid+1
         else:
+            # Answer lies in the left half
             high = mid-1
 
+#Program Starts here
 if __name__ == "__main__":
     nums = [5,6,6,9,9,9,0,0,2,3,3,3,3,4,4]
+    # Calling a main function.
     count = number_of_rotations(nums)
     print(count)
--- Binary Search/Binary Search Algorithem.py
+++ Binary Search/Binary Search Algorithem.py
@@ -1,7 +1,17 @@
+'''
+Question: Given an array of integers nums sorted in ascending order, find the starting and ending position of a given number. 
 
+Solution:
+1. The numbers are sorted in increasing order.
+2. We are looking for both the increasing order and the decreasing order.
+'''
+#This function will return the the 1st Position of the target value.
 def first_position(nums, target):
+
+    #It is a generic Binary Search Algorithm
     def binary_search(low, high, query):
         while low <= high:
+            # The middle position is the answer
             mid = (low+high)//2
             direction = condition(mid)
             if(direction == "found"):
@@ -11,6 +21,7 @@ def first_position(nums, target):
             else:
                 low = mid+1
 
+    #It is going to check whther the value is repeated second time or first time or t zeroeth position
     def condition(mid):
         if(nums[mid] == target):
             if(mid > 0 and nums[mid-1] == target):
@@ -22,10 +33,13 @@ def first_position(nums, target):
             return "left"
     return binary_search(0, len(nums)-1, target)
 
-
+#This function will return the the last Position of the target value.
 def last_position(nums, target):
+
+    #It is a generic Binary Search Algorithm
     def binary_search(low, high, query):
         while low <= high:
+            # The middle position is the answer
             mid = (low+high)//2
             direction = condition(mid)
             if(direction == "found"):
@@ -35,6 +49,7 @@ def last_position(nums, target):
             else:
                 low = mid+1
 
+    #It is going to check whther the value is repeated second time or first time or t zeroeth position
     def condition(mid):
         if(nums[mid] == target):
             if(mid < (len(nums)-1) and nums[mid+1] == target):
@@ -46,11 +61,14 @@ def last_position(nums, target):
             return "left"
     return binary_search(0, len(nums)-1, target)
 
+#This function first go to the First Position function to find te 1st position and then it will move to Last position
 def first_last_position(nums, target):
     return first_position(nums, target), last_position(nums, target)
 
+#Program Starts here
 if __name__ == "__main__":
     nums = [5,7,7,8,8,10]
     target = 8
+    # Calling a main function and storing the returned value in a list.
     arr = list(first_last_position(nums, target))
     print(arr)
