451d78f21d631a80bc77a5e2701a1fa3f6fab8af
Meghal Agarwal <meghal.agarwal@konverge.ai>
2022-04-14 18:31:26 +0530

    Longest Common Sequence

---
+++ Recursion and Dynamic Programming/Longest Common Sequence using Dynamic Programing.py
@@ -0,0 +1,39 @@
+'''
+Dynamic Programing works on the basis of the Matrix
+
+                    - s e r e n d i p i t o u s
+                -
+                p   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                r   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                e   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                c   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                i   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                p   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                i   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                t   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                a   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                t   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                i   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                o   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                n   0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+Time Complexity of Dynamic Programing if O(n1 * n2)
+'''
+
+def lcq_dp(seq1, seq2):
+    n1, n2 = len(seq1), len(seq2)
+    results = [[0 for _ in range(n2+1)] for _ in range(n1+1)]
+    for i in range(n1):
+        for j in range(n2):
+            if seq1[i] == seq2[j]:
+                results[i+1][j+1] = 1 + results[i][j]
+            else:
+                results[i+1][j+1] = max(results[i][j+1], results[i+1][j])
+    return results[-1][-1]
+
+# Program Starts Here
+if __name__ == "__main__":
+    seq1, seq2 = 'serendipitous', 'precipitation'
+
+    lcs_count = lcq_dp(seq1, seq2)
+    print(lcs_count)
--- Recursion and Dynamic Programming/Longest Common Sequence.py
+++ Recursion and Dynamic Programming/Longest Common Sequence.py
@@ -5,23 +5,25 @@ Lists, tuples and ranges are some common sequence types in Python.
 
 A "subsequence" is a sequence obtained by deleting zero or more elements from another sequence.
 For example, "edpt" is a subsequence of "serendipitous".
-Time Complexity of LCS is O(2^m+n)
+Time Complexity of LCS is O(n^m)
 '''
 
 def lcs_memo (seq1, seq2):
     memo = {}
     
     def recurse(idx1=0, idx2=0):
-        key=(idx1, idx2)
+        key=(idx1, idx2)        
         if(key in memo):
             return memo[key]
         elif(idx1 == len(seq1) or idx2 == len(seq2)):
             memo[key] = 0
         elif(seq1[idx1] == seq2[idx2]):
+            print(memo)
             memo[key] = 1 + recurse(idx1+1, idx2+1)
-        else:
+        else:            
             memo[key] = max(recurse(idx1+1, idx2), recurse(idx1, idx2+1))
         return memo[key]
+    
     return recurse(0, 0)
 
 if __name__ == "__main__":
