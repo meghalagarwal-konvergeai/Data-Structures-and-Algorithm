a16b58ccd52be85f32cdb8a073a72f752f5411a5
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-04-20 17:01:09 +0530

    Mo=inor Changes

--- Graph Algorithms (BFS, DFS, Shortest Paths) using Python/Breadth First Search.py
+++ Graph Algorithms (BFS, DFS, Shortest Paths) using Python/Breadth First Search.py
@@ -20,33 +20,28 @@ class Graph:
         return self.__repr__()
 
 # BFS Algorithm Starts Here
-def bfs(graph, root):
+def bfs(graph, source):
+    visited = [False] * len(graph.data)
     queue = []
-    discovered = [False] * len(graph.data)
-    distance = [None] * len(graph.data)
-    parent = [None] * len(graph.data)
-    discovered[root] = True
-    queue.append(root)
-    distance[root] = 0
-    idx = 0
-
-    while idx < len(queue):
-        #Dequeuing
-        current = queue[idx]
-        idx += 1
-
-        # check all the edges of current
-        for node in graph.data[current]:
-            if not discovered[node]:
-                # Distance from the Parent Node
-                distance[node] = 1 + distance[current]
-                # Returns the Parent
-                parent[node] = current
-                discovered[node] = True
-                queue.append(node)
-
-    return queue, distance, parent
+    
+    visited[source] = True    
+    queue.append(source)
+    i = 0
+    
+    while i < len(queue):
+        for v in graph.data[queue[i]]:
+            if not visited[v]:
+                visited[v] = True
+                queue.append(v)
+        i += 1
+        
+    return queue
 
 if __name__ == "__main__":
     num_nodes = 5
     graph = [(0,1),(0,4),(1,2),(1,3),(1,4),(2,3),(3,4)]
+
+    g1 = Graph(num_nodes, graph)
+    bf_search = bfs(g1, 3)
+    print(g1)
+    print(bf_search)
